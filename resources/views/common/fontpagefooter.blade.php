3b0f95dac40466cd376722ed3c5d9e49d187e623
ashikXpress <ctashiqkhan@gmail.com>
2017-12-04 03:01:48 +0600

    3rd day 1.1

--- resources/views/common/fontpagefooter.blade.php
+++ resources/views/common/fontpagefooter.blade.php
@@ -4,13 +4,10 @@
 <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
 <script src="{{asset('js/lightbox.js')}}"></script>
 <script src="{{asset('js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
-
 <script type="text/javascript">
-
- (function($){
+    (function($){
         $(window).on("load",function(){
             $(".inner-area").mCustomScrollbar({
-
                 scrollButtons:{enable:true,scrollType:"stepped"},
                 keyboard:{scrollType:"stepped"},
                 mouseWheel:{scrollAmount:188,normalizeDelta:true},
@@ -18,20 +15,10 @@
                 autoExpandScrollbar:true,
                 snapAmount:188,
                 snapOffset:20
-
             });
-
-
         });
-
-     $(window).on("load",function(){
-
-         $(".preloader").fadeOut(5000);
-
-
-
-     });
-
+        $(window).on("load",function(){
+            $(".preloader").fadeOut(5000);
+        });
     })(jQuery);
-
 </script>
